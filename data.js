f32112a873b403aa524c6985b9e748e29e0ab48b
KishorBalgi <KishorBalgi@users.noreply.github.com>
2021-10-01 21:10:48 +0530

    Added Profile Details

--- data.js
+++ data.js
@@ -446,7 +446,8 @@ const team = [
     name: "Bhargavi Nayak",
     college: "NMAM Institute of Technology",
     batch: 2019,
-    quote: "Don't rest, start the HacktoberFest with full on zest and get the best out of it!",
+    quote:
+      "Don't rest, start the HacktoberFest with full on zest and get the best out of it!",
     photo: "https://i.ibb.co/XL6mnp6/IMG-20201205-WA0017.jpg",
     github: "https://github.com/Bhargavi09",
   },
@@ -1122,7 +1123,8 @@ const team = [
     college: "Shri Jayachamarajendra College Of Engineering",
     batch: 2019,
     quote: "You are entitled to the actions, never to its fruits",
-    photo: "https://media-exp1.licdn.com/dms/image/C5603AQHJYf6bBUeY1Q/profile-displayphoto-shrink_800_800/0/1616585603947?e=1638403200&v=beta&t=mcM0ZU9kaSfiBKenRQ7Mfj5gSvqPe9u7qvwMdYV5EHY",
+    photo:
+      "https://media-exp1.licdn.com/dms/image/C5603AQHJYf6bBUeY1Q/profile-displayphoto-shrink_800_800/0/1616585603947?e=1638403200&v=beta&t=mcM0ZU9kaSfiBKenRQ7Mfj5gSvqPe9u7qvwMdYV5EHY",
     github: "https://github.com/RajathPrabhu221",
   },
   {
@@ -1154,12 +1156,12 @@ const team = [
   },
   {
     teamname: "Vivaan",
-    name: "Your Name",
-    college: "Your College",
-    batch: 2016,
-    quote: "Your quote",
-    photo: "Your image url",
-    github: "Your github profile url",
+    name: "Kishor Balgi",
+    college: "K L S Gogte Institute of Technology",
+    batch: 2020,
+    quote: "Always Learning!",
+    photo: "https://i.ibb.co/Fs4B3Wd/IMG20210822111231.jpg",
+    github: "https://github.com/KishorBalgi",
   },
   {
     teamname: "Vivaan",
